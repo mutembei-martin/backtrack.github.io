3ffb454cf108c31dda2772da1dd01ce951457b73
mutembei-martin <walkerrigby@gmail.com>
2017-11-09 20:47:41 +0300

    eminem

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+
+/* HTML document is loaded. DOM is ready. 
+-------------------------------------------*/
+$(function(){
+
+  // ------- WOW ANIMATED ------ //
+  wow = new WOW(
+  {
+    mobile: false
+  });
+  wow.init();
+
+
+});
+
